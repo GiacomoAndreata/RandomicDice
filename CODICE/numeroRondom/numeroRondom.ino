2f119497b589821d5db951b1af47b4630b8c862c
GiacomoAndreata <giacomo.andreata@studenti.unimi.it>
2020-08-26 17:13:12 +0200

    Update numeroRondom.ino
    include library MPU6050_tockn.h

--- CODICE/numeroRondom/numeroRondom.ino
+++ CODICE/numeroRondom/numeroRondom.ino
@@ -1,42 +1,38 @@
+#include <MPU6050_tockn.h>
 #include<Wire.h>
 
 //giroscopio
-const int MPU=0x68;  // I2C address of the MPU-6050
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+MPU6050 mpu6050(Wire);
+int AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
 
 int numberRandom;
 
 void setup(){
-  Serial.begin(9600);
-  
+  Serial.begin(115200);
+  //randomSeed(???);
   //giroscopio
   Wire.begin();
-  Wire.beginTransmission(MPU);
-  Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set zero (wakes up the MPU-6050)
-  Wire.endTransmission(true);
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
 
 }
 void loop(){
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU,14,true);  // request a total of 14 registers
+  mpu6050.update();
   
-  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  AcX=mpu6050.getAccX();  //    
+  AcY=mpu6050.getAccY();  //
+  AcZ=mpu6050.getAccZ();  //
+  Tmp=mpu6050.getTemp();  //
+  GyX=mpu6050.getGyroX();  //
+  GyY=mpu6050.getGyroY();  //
+  GyZ=mpu6050.getGyroZ();  //
 
-  Serial.print("X: "); Serial.println(AcX);
+  Serial.print("X: "); Serial.println(mpu6050.getGyroX());
+  Serial.print("GyX:"); Serial.println(GyX);
   
-  randomSeed(AcX);
-  numberRandom = random(10);
-  Serial.print("Numero: "); Serial.println(numberRandom);
-
+  
+  //numberRandom = random(10);
   delay(1000);
   
 }
